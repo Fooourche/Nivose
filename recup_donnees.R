736e84d463c2f52c73671dc4b486370aea0956b4
Fooourche <fabien.rinaldi@gmail.com>
2016-11-11 15:06:32 +0100

    Recuperation des donnees

--- recup_donnees.R
+++ recup_donnees.R
@@ -1,2 +1,16 @@
 #adresse
-#https://donneespubliques.meteofrance.fr/?fond=donnee_libre&prefixe=Txt%2FNivo%2Fnivo&extension=csv&date=20161110
+#https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Nivo/nivo.20161110.csv
+
+#1 recuperer les donnees
+url <- paste0('https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Nivo/nivo.',format(Sys.Date()-1,"%Y%m%d"),'.csv')
+download.file(url,destfile = 'nivose.csv')
+
+#2 lecture & decodage
+catalogue_nivose <- read.csv2('postesNivo.csv',sep = ',',stringsAsFactors = FALSE)
+datas_nivose <- read.csv2(file = 'nivose.csv',stringsAsFactors = FALSE)
+
+
+#4 affichage
+
+
+
